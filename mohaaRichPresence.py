cc0539a349204cce0c06086b200c2042bc7cb916
Appelpitje <laurens.depaepe@gmail.com>
2024-01-10 13:14:26 +0100

    Update mohaaRichPresence.py
    fix for cpu lag spikes

--- mohaaRichPresence.py
+++ mohaaRichPresence.py
@@ -7,42 +7,47 @@ import time
 def readMOHAA():
     try:
         print("Looking for MOHAA.")
-        # Find the MOH console window
-        try:
-            hwnd = win32gui.FindWindow("MOHTA WinConsole", None)
-            win32gui.ShowWindow(hwnd, win32con.SW_SHOW)
+        hwnd = win32gui.FindWindow("MOHTA WinConsole", None)
+        if hwnd:
+            # Only show the window if it's not already visible.
+            if not win32gui.IsWindowVisible(hwnd):
+                win32gui.ShowWindow(hwnd, win32con.SW_SHOW)
+
             clearBtn = win32gui.FindWindowEx(hwnd, None, "Button", "clear")
             win32gui.SendMessage(hwnd, win32con.WM_COMMAND, 1, clearBtn)
-        except Exception as e:
-            print("Error[win32gui]: " + str(e))
-        win32gui.SendMessage(hwnd, win32con.WM_COMMAND, 1, clearBtn)
 
-        #Set cmdline with serverinfo
-        cmdline = win32gui.GetDlgItem(hwnd, 101)
-        win32gui.SendMessage(cmdline, win32con.WM_SETTEXT, None, "clientinfo")
-        win32gui.PostMessage(cmdline, win32con.WM_KEYDOWN, win32con.VK_RETURN, 0)
+            # Set cmdline with serverinfo
+            cmdline = win32gui.GetDlgItem(hwnd, 101)
+            win32gui.SendMessage(cmdline, win32con.WM_SETTEXT, None, "clientinfo")
+            win32gui.PostMessage(cmdline, win32con.WM_KEYDOWN, win32con.VK_RETURN, 0)
 
-        #Get console info
-        console = win32gui.GetDlgItem(hwnd, 100)
-        buffer = win32gui.PyMakeBuffer(25555)
-        length = win32gui.SendMessage(console, win32con.WM_GETTEXT, 25555, buffer)
-        result = buffer[0:length * 2].tobytes().decode("UTF-16")
-        time.sleep(2)
-        # Click the clear button
-        win32gui.SendMessage(hwnd, win32con.WM_COMMAND, 1, clearBtn)
+            # Get console info
+            console = win32gui.GetDlgItem(hwnd, 100)
+            buffer = win32gui.PyMakeBuffer(25555)
+            length = win32gui.SendMessage(console, win32con.WM_GETTEXT, 25555, buffer)
+            result = buffer[0:length * 2].tobytes().decode("UTF-16")
 
-        # split on newline
-        splitted = result.splitlines()
+            # Click the clear button
+            win32gui.SendMessage(hwnd, win32con.WM_COMMAND, 1, clearBtn)
 
-        for index, value in enumerate(splitted):
-            if "Server: " in value:
-                print("Found server! " + value[8:])
-                return value[8:]
+            # Split on newline
+            splitted = result.splitlines()
+
+            for index, value in enumerate(splitted):
+                if "Server: " in value:
+                    print("Found server! " + value[8:])
+                    return value[8:]
+                    
+        else:
+            print("MOHAA window not found.")
 
     except Exception as e:
         print("Error[MOHSH]: " + str(e))
 
 def getServerDetails(server):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.settimeout(5)
+
     try:
         ipport = server.split(':')
         IP = ipport[0]
@@ -50,28 +55,24 @@ def getServerDetails(server):
         cmd = "getstatus"
         serverinfo = []
 
-        try:
-            sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-            sock.settimeout(5)
+        sock.connect((IP, port))
+        sock.send(b"\xFF\xFF\xFF\xFF\x02" + str.encode(cmd))
+        received = sock.recv(65565)
 
-            sock.connect((IP, port))
-            sock.send(b"\xFF\xFF\xFF\xFF\x02" + str.encode(cmd))
-            received = sock.recv(65565)
-        except Exception as e:
-            print("Error[socket]: " + str(e))
         r = received.decode("ISO-8859-1").split("\\")
         servernameindex = r.index("sv_hostname")
         mapindex = r.index("mapname")
 
-        serverinfo.append(r[servernameindex+1])
-        serverinfo.append(r[mapindex+1])
-        print("Translating IP to servername: " + r[servernameindex+1])
-        sock.close()
+        serverinfo.append(r[servernameindex + 1])
+        serverinfo.append(r[mapindex + 1])
+        print("Translating IP to servername: " + r[servernameindex + 1])
+
         return serverinfo
 
     except Exception as e:
         print("Error[network]: " + str(e))
         print("Connection timeout to server.")
+    finally:
         sock.close()
 
 def discordRP():
@@ -80,18 +81,26 @@ def discordRP():
         RPC = Presence(client_id)
         print("Connecting to Discord Rich Presence.")
         RPC.connect()
-        print("Succesfully connnected!")
+        print("Successfully connected!")
+        
+        last_update_time = 0
+        update_interval = 60  # Update every 60 seconds
+
+        while True:
+            current_time = time.time()
+            if current_time - last_update_time > update_interval:
+                try:
+                    mohaaserver = readMOHAA()
+                    if mohaaserver:
+                        serverinfo = getServerDetails(mohaaserver)
+                        RPC.update(state=serverinfo[1], details=serverinfo[0], large_image="moh_icon", small_image=serverinfo[1][:3])
+                except Exception as e:
+                    print("Error[Discord]: " + str(e))
+                finally:
+                    last_update_time = current_time
+            time.sleep(0.1)  # Sleep for a shorter time to be more responsive
     except Exception as e:
         print("Couldn't connect to Discord Rich Presence, error: \n" + str(e))
-
-    while True:  # The presence will stay on as long as the program is running
-        try:
-            mohaaserver = readMOHAA()
-            serverinfo = getServerDetails(mohaaserver)
-            RPC.update(state=serverinfo[1], details=serverinfo[0], large_image="moh_icon", small_image=serverinfo[1][:3], ) # Set the presence
-        except Exception as e:
-            print("Error[Discord]: " + str(e))
-        time.sleep(60)
 
 if __name__ == '__main__':
     discordRP()
